b69d797682deeeaccc2468f55e0742976e8b4a35
joaquimg <joaquimdgarcia@gmail.com>
2018-01-17 01:08:36 -0200

    enable quad tests but needs queries

--- test/runtests.jl
+++ test/runtests.jl
@@ -16,11 +16,11 @@ const MOIXPR = MathOptInterfaceXpress
         MOIT.linear12test(solverf, linconfig_nocertificate)
     end
 
-    # @testset "Quadratic tests" begin
-    #     quadconfig = MOIT.TestConfig(duals = false)
-    #     solverf() = MOIXPR.XpressSolverInstance()
-    #     MOIT.contquadratictest(solverf, quadconfig)
-    # end
+    @testset "Quadratic tests" begin
+        quadconfig = MOIT.TestConfig(atol = 1e-5, rtol = 1e-5, duals = false)
+        solverf() = MOIXPR.XpressSolverInstance()
+        MOIT.contquadratictest(solverf, quadconfig)
+    end
 
     @testset "Linear Conic tests" begin
         linconfig = MOIT.TestConfig()
