f17d45c6eda0e5867755d4c0b9a7beb10391be7d
joaquimg <joaquimdgarcia@gmail.com>
2018-02-17 01:37:31 -0200

    update to match MOI

--- src/MathOptInterfaceXpress.jl
+++ src/MathOptInterfaceXpress.jl
@@ -1,9 +1,6 @@
 module MathOptInterfaceXpress
 
-import Base.show, Base.copy
-
-# Standard LP interface
-# importall MathProgBase.SolverInterface
+export XpressOptimizer
 
 using Xpress
 const XPR = Xpress
@@ -12,26 +9,10 @@ const MOI = MathOptInterface
 using LinQuadOptInterface
 const LQOI = LinQuadOptInterface
 
-import Xpress.Model
-Xpress.Model(::Void) = Xpress.Model()
-mutable struct XpressSolverInstance <: LQOI.LinQuadSolverInstance
-    LQOI.@LinQuadSolverInstanceBase
-end
-function XpressSolverInstance(;kwargs...)
-
-    env = nothing
-    instance = XpressSolverInstance(
-        (LQOI.@LinQuadSolverInstanceBaseInit)...,
-    )
-    for (name,value) in kwargs
-        XPR.setparam!(instance.inner, XPR.XPRS_CONTROLS_DICT[name], value)
-    end
-    # csi.inner.mipstart_effort = s.mipstart_effortlevel
-    # if s.logfile != ""
-    #     LQOI.lqs_setlogfile!(env, s.logfile)
-    # end
-    return instance
-end
+const SUPPORTED_OBJECTIVES = [
+    LQOI.Linear,
+    LQOI.Quad
+]
 
 const SUPPORTED_CONSTRAINTS = [
     (LQOI.Linear, LQOI.EQ),
@@ -47,8 +28,8 @@ const SUPPORTED_CONSTRAINTS = [
     (LQOI.SinVar, LQOI.IV),
     (LQOI.SinVar, MOI.ZeroOne),
     (LQOI.SinVar, MOI.Integer),
-    (LQOI.VecVar, MOI.SOS1),
-    (LQOI.VecVar, MOI.SOS2),
+    (LQOI.VecVar, LQOI.SOS1),
+    (LQOI.VecVar, LQOI.SOS2),
     (LQOI.VecVar, MOI.Nonnegatives),
     (LQOI.VecVar, MOI.Nonpositives),
     (LQOI.VecVar, MOI.Zeros),
@@ -57,13 +38,36 @@ const SUPPORTED_CONSTRAINTS = [
     (LQOI.VecLin, MOI.Zeros)
 ]
 
-const SUPPORTED_OBJECTIVES = [
-    LQOI.Linear,
-    LQOI.Quad
-]
+mutable struct XpressOptimizer <: LQOI.LinQuadOptimizer
+    LQOI.@LinQuadOptimizerBase
+    params::Dict{Any,Any}
+    XpressOptimizer(::Void) = new()    
+end
 
-LQOI.lqs_supported_constraints(s::XpressSolverInstance) = SUPPORTED_CONSTRAINTS
-LQOI.lqs_supported_objectives(s::XpressSolverInstance) = SUPPORTED_OBJECTIVES
+LQOI.LinQuadModel(::Type{XpressOptimizer},env) = XPR.Model()
+
+function XpressOptimizer(;kwargs...)
+
+    env = nothing
+    m = XpressOptimizer(nothing)
+    m.params = Dict{Any,Any}()
+    MOI.empty!(m)
+    for (name,value) in kwargs
+        m.params[name] = value
+        XPR.setparam!(m.inner, XPR.XPRS_CONTROLS_DICT[name], value)
+    end
+    return m
+end
+
+function MOI.empty!(m::XpressOptimizer) 
+    MOI.empty!(m,nothing)
+    for (name,value) in m.params
+        XPR.setparam!(m.inner, XPR.XPRS_CONTROLS_DICT[name], value)
+    end
+end
+
+LQOI.lqs_supported_constraints(s::XpressOptimizer) = SUPPORTED_CONSTRAINTS
+LQOI.lqs_supported_objectives(s::XpressOptimizer) = SUPPORTED_OBJECTIVES
 
 #=
     inner wrapper
@@ -78,11 +82,11 @@ LQOI.lqs_supported_objectives(s::XpressSolverInstance) = SUPPORTED_OBJECTIVES
 
 # LQOI.lqs_setparam!(env, name, val)
 # TODO fix this one
-LQOI.lqs_setparam!(instance::XpressSolverInstance, name, val) = XPR.setparam!(instance.inner, XPR.XPRS_CONTROLS_DICT[name], val)
+LQOI.lqs_setparam!(instance::XpressOptimizer, name, val) = XPR.setparam!(instance.inner, XPR.XPRS_CONTROLS_DICT[name], val)
 
 # LQOI.lqs_setlogfile!(env, path)
 # TODO fix this one
-LQOI.lqs_setlogfile!(instance::XpressSolverInstance, path) = XPR.setlogfile(instance.inner, path::String)
+LQOI.lqs_setlogfile!(instance::XpressOptimizer, path) = XPR.setlogfile(instance.inner, path::String)
 
 # LQOI.lqs_getprobtype(m)
 # TODO - consider removing, apparently useless
@@ -92,35 +96,38 @@ LQOI.lqs_setlogfile!(instance::XpressSolverInstance, path) = XPR.setlogfile(inst
 =#
 
 cintvec(v::Vector) = convert(Vector{Int32}, v)
-cdoublevec(v::Vector) = convert(Vector{Float64}, v)
+
+MOI.canset(::XpressOptimizer, ::MOI.ObjectiveFunction{MOI.ScalarAffineFunction{Float64}}) = true
+MOI.canset(::XpressOptimizer, ::MOI.ObjectiveFunction{MOI.ScalarQuadraticFunction{Float64}}) = true
+
 
 # LQOI.lqs_chgbds!(m, colvec, valvec, sensevec)
-LQOI.lqs_chgbds!(instance::XpressSolverInstance, colvec, valvec, sensevec) = XPR.chgbounds!(instance.inner, cintvec(colvec), sensevec, valvec)
+LQOI.lqs_chgbds!(instance::XpressOptimizer, colvec, valvec, sensevec) = XPR.chgbounds!(instance.inner, cintvec(colvec), sensevec, valvec)
 
 # LQOI.lqs_getlb(m, col)
-LQOI.lqs_getlb(instance::XpressSolverInstance, col) = XPR.get_lb(instance.inner, col, col)[1]
+LQOI.lqs_getlb(instance::XpressOptimizer, col) = XPR.get_lb(instance.inner, col, col)[1]
 # LQOI.lqs_getub(m, col)
-LQOI.lqs_getub(instance::XpressSolverInstance, col) = XPR.get_ub(instance.inner, col, col)[1]
+LQOI.lqs_getub(instance::XpressOptimizer, col) = XPR.get_ub(instance.inner, col, col)[1]
 
 # LQOI.lqs_getnumrows(m)
-LQOI.lqs_getnumrows(instance::XpressSolverInstance) = XPR.num_linconstrs(instance.inner)
+LQOI.lqs_getnumrows(instance::XpressOptimizer) = XPR.num_linconstrs(instance.inner)
 
 # LQOI.lqs_addrows!(m, rowvec, colvec, coefvec, sensevec, rhsvec)
-LQOI.lqs_addrows!(instance::XpressSolverInstance, rowvec, colvec, coefvec, sensevec, rhsvec) = XPR.add_constrs!(instance.inner, rowvec, colvec, coefvec, sensevec, rhsvec)
+LQOI.lqs_addrows!(instance::XpressOptimizer, rowvec, colvec, coefvec, sensevec, rhsvec) = XPR.add_constrs!(instance.inner, rowvec, colvec, coefvec, sensevec, rhsvec)
 
 # LQOI.lqs_getrhs(m, rowvec)
-LQOI.lqs_getrhs(instance::XpressSolverInstance, row) = XPR.get_rhs(instance.inner, row, row)[1]  
+LQOI.lqs_getrhs(instance::XpressOptimizer, row) = XPR.get_rhs(instance.inner, row, row)[1]  
 
 # colvec, coef = LQOI.lqs_getrows(m, rowvec)
 # TODO improve
-function LQOI.lqs_getrows(instance::XpressSolverInstance, idx)
+function LQOI.lqs_getrows(instance::XpressOptimizer, idx)
     A = XPR.get_rows(instance.inner, idx, idx)'
     return A.rowval-1, A.nzval
 end
 
 # LQOI.lqs_getcoef(m, row, col) #??
 # TODO improve
-function LQOI.lqs_getcoef(instance::XpressSolverInstance, row, col) #??
+function LQOI.lqs_getcoef(instance::XpressOptimizer, row, col) #??
     A = XPR.get_rows(instance.inner, row, row)'
     cols = A.rowval
     vals = A.nzval
@@ -135,7 +142,7 @@ end
 
 # LQOI.lqs_chgcoef!(m, row, col, coef)
 # TODO SPLIT THIS ONE
-function LQOI.lqs_chgcoef!(instance::XpressSolverInstance, row, col, coef) 
+function LQOI.lqs_chgcoef!(instance::XpressOptimizer, row, col, coef) 
     if row == 0
         XPR.set_objcoeffs!(instance.inner, Int32[col], Float64[coef])
     elseif col == 0
@@ -146,37 +153,37 @@ function LQOI.lqs_chgcoef!(instance::XpressSolverInstance, row, col, coef)
 end
 
 # LQOI.lqs_delrows!(m, row, row)
-LQOI.lqs_delrows!(instance::XpressSolverInstance, rowbeg, rowend) = XPR.del_constrs!(instance.inner, cintvec(collect(rowbeg:rowend))) 
+LQOI.lqs_delrows!(instance::XpressOptimizer, rowbeg, rowend) = XPR.del_constrs!(instance.inner, cintvec(collect(rowbeg:rowend))) 
 
 # LQOI.lqs_chgctype!(m, colvec, typevec)
 # TODO fix types
-LQOI.lqs_chgctype!(instance::XpressSolverInstance, colvec, typevec) = XPR.chgcoltypes!(instance.inner, colvec, typevec)
+LQOI.lqs_chgctype!(instance::XpressOptimizer, colvec, typevec) = XPR.chgcoltypes!(instance.inner, colvec, typevec)
 
 # LQOI.lqs_chgsense!(m, rowvec, sensevec)
 # TODO fix types
-LQOI.lqs_chgsense!(instance::XpressSolverInstance, rowvec, sensevec) = XPR.set_rowtype!(instance.inner, rowvec, sensevec)
+LQOI.lqs_chgsense!(instance::XpressOptimizer, rowvec, sensevec) = XPR.set_rowtype!(instance.inner, rowvec, sensevec)
 
 const VAR_TYPE_MAP = Dict{Symbol,Cchar}(
     :CONTINUOUS => Cchar('C'),
     :INTEGER => Cchar('I'),
     :BINARY => Cchar('B')
 )
-LQOI.lqs_vartype_map(m::XpressSolverInstance) = VAR_TYPE_MAP
+LQOI.lqs_vartype_map(m::XpressOptimizer) = VAR_TYPE_MAP
 
 # LQOI.lqs_addsos(m, colvec, valvec, typ)
-LQOI.lqs_addsos!(instance::XpressSolverInstance, colvec, valvec, typ) = XPR.add_sos!(instance.inner, typ, colvec, valvec)
+LQOI.lqs_addsos!(instance::XpressOptimizer, colvec, valvec, typ) = XPR.add_sos!(instance.inner, typ, colvec, valvec)
 # LQOI.lqs_delsos(m, idx, idx)
-LQOI.lqs_delsos!(instance::XpressSolverInstance, idx1, idx2) = XPR.del_sos!(instance.inner, cintvec(collect(idx1:idx2)))
+LQOI.lqs_delsos!(instance::XpressOptimizer, idx1, idx2) = XPR.del_sos!(instance.inner, cintvec(collect(idx1:idx2)))
 
 const SOS_TYPE_MAP = Dict{Symbol,Symbol}(
     :SOS1 => :SOS1,#Cchar('1'),
     :SOS2 => :SOS2#Cchar('2')
 )
-LQOI.lqs_sertype_map(m::XpressSolverInstance) = SOS_TYPE_MAP
+LQOI.lqs_sertype_map(m::XpressOptimizer) = SOS_TYPE_MAP
 
 # LQOI.lqs_getsos(m, idx)
 # TODO improve getting processes
-function LQOI.lqs_getsos(instance::XpressSolverInstance, idx)
+function LQOI.lqs_getsos(instance::XpressOptimizer, idx)
     A, types = XPR.get_sos_matrix(instance.inner)
     line = A[idx,:] #sparse vec
     cols = line.nzind
@@ -186,13 +193,13 @@ function LQOI.lqs_getsos(instance::XpressSolverInstance, idx)
 end
 
 # LQOI.lqs_getnumqconstrs(m)
-LQOI.lqs_getnumqconstrs(instance::XpressSolverInstance) = XPR.num_qconstrs(instance.inner)
+LQOI.lqs_getnumqconstrs(instance::XpressOptimizer) = XPR.num_qconstrs(instance.inner)
 
 # LQOI.lqs_addqconstr(m, cols,coefs,rhs,sense, I,J,V)
-LQOI.lqs_addqconstr!(instance::XpressSolverInstance, cols,coefs,rhs,sense, I,J,V) = XPR.add_qconstr!(instance.inner, cols, coefs, I, J, V, sense, rhs)
+LQOI.lqs_addqconstr!(instance::XpressOptimizer, cols,coefs,rhs,sense, I,J,V) = XPR.add_qconstr!(instance.inner, cols, coefs, I, J, V, sense, rhs)
 
 # LQOI.lqs_chgrngval
-LQOI.lqs_chgrngval!(instance::XpressSolverInstance, rows, vals) = XPR.chg_rhsrange!(instance.inner, cintvec(rows), -vals)
+LQOI.lqs_chgrngval!(instance::XpressOptimizer, rows, vals) = XPR.chg_rhsrange!(instance.inner, cintvec(rows), -vals)
 
 const CTR_TYPE_MAP = Dict{Symbol,Cchar}(
     :RANGE => Cchar('R'),
@@ -200,21 +207,21 @@ const CTR_TYPE_MAP = Dict{Symbol,Cchar}(
     :UPPER => Cchar('U'),
     :EQUALITY => Cchar('E')
 )
-LQOI.lqs_ctrtype_map(m::XpressSolverInstance) = CTR_TYPE_MAP
+LQOI.lqs_ctrtype_map(m::XpressOptimizer) = CTR_TYPE_MAP
 
 #=
     Objective
 =#
 
 # LQOI.lqs_copyquad(m, intvec,intvec, floatvec) #?
-function LQOI.lqs_copyquad!(instance::XpressSolverInstance, I, J, V)
+function LQOI.lqs_copyquad!(instance::XpressOptimizer, I, J, V)
     XPR.delq!(instance.inner)
     XPR.add_qpterms!(instance.inner, I, J, V)
     return nothing
 end
 
 # LQOI.lqs_chgobj(m, colvec,coefvec)
-function LQOI.lqs_chgobj!(instance::XpressSolverInstance, colvec, coefvec) 
+function LQOI.lqs_chgobj!(instance::XpressOptimizer, colvec, coefvec) 
     nvars = XPR.num_vars(instance.inner)
     obj = zeros(Float64, nvars)
 
@@ -228,7 +235,7 @@ end
 
 # LQOI.lqs_chgobjsen(m, symbol)
 # TODO improve min max names
-function LQOI.lqs_chgobjsen!(instance::XpressSolverInstance, symbol)
+function LQOI.lqs_chgobjsen!(instance::XpressOptimizer, symbol)
     if symbol == :Min
         XPR.set_sense!(instance.inner, :minimize)
     else
@@ -238,10 +245,10 @@ end
     
 
 # LQOI.lqs_getobj(m)
-LQOI.lqs_getobj(instance::XpressSolverInstance) = XPR.get_obj(instance.inner)
+LQOI.lqs_getobj(instance::XpressOptimizer) = XPR.get_obj(instance.inner)
 
 # lqs_getobjsen(m)
-function LQOI.lqs_getobjsen(instance::XpressSolverInstance)
+function LQOI.lqs_getobjsen(instance::XpressOptimizer)
     s = XPR.model_sense(instance.inner)
     if s == :maximize
         return MOI.MaxSense
@@ -255,16 +262,16 @@ end
 =#
 
 # LQOI.lqs_getnumcols(m)
-LQOI.lqs_getnumcols(instance::XpressSolverInstance) = XPR.num_vars(instance.inner)
+LQOI.lqs_getnumcols(instance::XpressOptimizer) = XPR.num_vars(instance.inner)
 
 # LQOI.lqs_newcols!(m, int)
-LQOI.lqs_newcols!(instance::XpressSolverInstance, int) = XPR.add_cvars!(instance.inner, zeros(int))
+LQOI.lqs_newcols!(instance::XpressOptimizer, int) = XPR.add_cvars!(instance.inner, zeros(int))
 
 # LQOI.lqs_delcols!(m, col, col)
-LQOI.lqs_delcols!(instance::XpressSolverInstance, col, col2) = XPR.del_vars!(instance.inner, col)
+LQOI.lqs_delcols!(instance::XpressOptimizer, col, col2) = XPR.del_vars!(instance.inner, col)
 
 # LQOI.lqs_addmipstarts(m, colvec, valvec)
-function LQOI.lqs_addmipstarts!(instance::XpressSolverInstance, colvec, valvec) 
+function LQOI.lqs_addmipstarts!(instance::XpressOptimizer, colvec, valvec) 
     x = zeros(XPR.num_vars(instance.inner))
     for i in eachindex(colvec)
         x[colvec[i]] = valvec[i]
@@ -276,16 +283,16 @@ end
 =#
 
 # LQOI.lqs_mipopt!(m)
-LQOI.lqs_mipopt!(instance::XpressSolverInstance) = XPR.mipoptimize(instance.inner)
+LQOI.lqs_mipopt!(instance::XpressOptimizer) = XPR.mipoptimize(instance.inner)
 
 # LQOI.lqs_qpopt!(m)
-LQOI.lqs_qpopt!(instance::XpressSolverInstance) = LQOI.lqs_lpopt!(instance)
+LQOI.lqs_qpopt!(instance::XpressOptimizer) = LQOI.lqs_lpopt!(instance)
 
 # LQOI.lqs_lpopt!(m)
-LQOI.lqs_lpopt!(instance::XpressSolverInstance) = XPR.lpoptimize(instance.inner)
+LQOI.lqs_lpopt!(instance::XpressOptimizer) = XPR.lpoptimize(instance.inner)
 
 # LQOI.lqs_terminationstatus(m)
-function LQOI.lqs_terminationstatus(instance::XpressSolverInstance)
+function LQOI.lqs_terminationstatus(instance::XpressOptimizer)
     stat_lp = XPR.get_lp_status2(instance.inner)
     if XPR.is_mip(instance.inner)
         stat_mip = XPR.get_mip_status2(instance.inner)
@@ -361,7 +368,7 @@ function LQOI.lqs_terminationstatus(instance::XpressSolverInstance)
     end
 end
 
-function xprsmoi_stopstatus(instance::XpressSolverInstance)
+function xprsmoi_stopstatus(instance::XpressOptimizer)
     ss = XPR.get_stopstatus(instance.inner)
     if ss == XPR.StopTimeLimit
         return MOI.TimeLimit
@@ -383,7 +390,7 @@ function xprsmoi_stopstatus(instance::XpressSolverInstance)
     return MOI.OtherError
 end
 
-function LQOI.lqs_primalstatus(instance::XpressSolverInstance)
+function LQOI.lqs_primalstatus(instance::XpressOptimizer)
     if XPR.is_mip(instance.inner)
         stat_mip = XPR.get_mip_status2(instance.inner)
         if stat_mip in [XPR.MIP_Solution, XPR.MIP_Optimal]
@@ -410,7 +417,7 @@ function LQOI.lqs_primalstatus(instance::XpressSolverInstance)
         end
     end
 end
-function LQOI.lqs_dualstatus(instance::XpressSolverInstance) 
+function LQOI.lqs_dualstatus(instance::XpressOptimizer) 
     if XPR.is_mip(instance.inner)
         return MOI.UnknownResultStatus
     else
@@ -430,10 +437,10 @@ end
 
 
 # LQOI.lqs_getx!(m, place)
-LQOI.lqs_getx!(instance::XpressSolverInstance, place) = XPR.get_solution!(instance.inner, place)
+LQOI.lqs_getx!(instance::XpressOptimizer, place) = XPR.get_solution!(instance.inner, place)
 
 # LQOI.lqs_getax!(m, place)
-function LQOI.lqs_getax!(instance::XpressSolverInstance, place)
+function LQOI.lqs_getax!(instance::XpressOptimizer, place)
     XPR.get_slack_lin!(instance.inner, place)
     rhs = XPR.get_rhs(instance.inner)
     for i in eachindex(place)
@@ -442,47 +449,47 @@ function LQOI.lqs_getax!(instance::XpressSolverInstance, place)
     nothing
 end
 # LQOI.lqs_getdj!(m, place)
-LQOI.lqs_getdj!(instance::XpressSolverInstance, place) = XPR.get_reducedcost!(instance.inner, place)
+LQOI.lqs_getdj!(instance::XpressOptimizer, place) = XPR.get_reducedcost!(instance.inner, place)
 
 # LQOI.lqs_getpi!(m, place)
-LQOI.lqs_getpi!(instance::XpressSolverInstance, place) = XPR.get_dual_lin!(instance.inner, place)
+LQOI.lqs_getpi!(instance::XpressOptimizer, place) = XPR.get_dual_lin!(instance.inner, place)
 
 # LQOI.lqs_getobjval(m)
-LQOI.lqs_getobjval(instance::XpressSolverInstance) = XPR.get_objval(instance.inner)
+LQOI.lqs_getobjval(instance::XpressOptimizer) = XPR.get_objval(instance.inner)
 
 # LQOI.lqs_getbestobjval(m)
-LQOI.lqs_getbestobjval(instance::XpressSolverInstance) = XPR.get_mip_objval(instance.inner)
+LQOI.lqs_getbestobjval(instance::XpressOptimizer) = XPR.get_mip_objval(instance.inner)
 
 # LQOI.lqs_getmiprelgap(m)
-function LQOI.lqs_getmiprelgap(instance::XpressSolverInstance)
+function LQOI.lqs_getmiprelgap(instance::XpressOptimizer)
     L = XPR.get_mip_objval(instance.inner)
     U = XPR.get_bestbound(instance.inner)
     return abs(U-L)/U
 end
 
 # LQOI.lqs_getitcnt(m)
-LQOI.lqs_getitcnt(instance::XpressSolverInstance)  = XPR.get_simplex_iter_count(instance.inner)
+LQOI.lqs_getitcnt(instance::XpressOptimizer)  = XPR.get_simplex_iter_count(instance.inner)
 
 # LQOI.lqs_getbaritcnt(m)
-LQOI.lqs_getbaritcnt(instance::XpressSolverInstance) = XPR.get_barrier_iter_count(instance.inner)
+LQOI.lqs_getbaritcnt(instance::XpressOptimizer) = XPR.get_barrier_iter_count(instance.inner)
 
 # LQOI.lqs_getnodecnt(m)
-LQOI.lqs_getnodecnt(instance::XpressSolverInstance) = XPR.get_node_count(instance.inner)
+LQOI.lqs_getnodecnt(instance::XpressOptimizer) = XPR.get_node_count(instance.inner)
 
 # LQOI.lqs_dualfarkas(m, place)
-LQOI.lqs_dualfarkas!(instance::XpressSolverInstance, place) = XPR.getdualray!(instance.inner, place)
+LQOI.lqs_dualfarkas!(instance::XpressOptimizer, place) = XPR.getdualray!(instance.inner, place)
 
 # LQOI.lqs_getray(m, place)
-LQOI.lqs_getray!(instance::XpressSolverInstance, place) = XPR.getprimalray!(instance.inner, place)
+LQOI.lqs_getray!(instance::XpressOptimizer, place) = XPR.getprimalray!(instance.inner, place)
 
 
-MOI.free!(m::XpressSolverInstance) = XPR.free_model(m.onner)
+MOI.free!(m::XpressOptimizer) = XPR.free_model(m.onner)
 
 """
-    writeproblem(m::AbstractSolverInstance, filename::String)
+    writeproblem(m: :MOI.AbstractOptimizer, filename::String)
 Writes the current problem data to the given file.
 Supported file types are solver-dependent.
 """
-writeproblem(instance::XpressSolverInstance, filename::String, flags::String="") = XPR.write_model(instance.inner, filename, flags)
+writeproblem(instance::XpressOptimizer, filename::String, flags::String="") = XPR.write_model(instance.inner, filename, flags)
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,40 +1,40 @@
-using Xpress, Base.Test, MathOptInterface, MathOptInterfaceTests, MathOptInterfaceXpress
+using Xpress, Base.Test, MathOptInterface, MathOptInterface.Test, MathOptInterfaceXpress
 
-const MOIT = MathOptInterfaceTests
+const MOIT = MathOptInterface.Test
 const MOIXPR = MathOptInterfaceXpress
 
 @testset "MathOptInterfaceXpress" begin
     @testset "Linear tests" begin
         linconfig = MOIT.TestConfig()
-        solverf() = MOIXPR.XpressSolverInstance()
-        MOIT.contlineartest(solverf , linconfig, ["linear12","linear8a","linear8b","linear8c"])
+        solver = XpressOptimizer()
+        MOIT.contlineartest(solver , linconfig, ["linear12","linear8a","linear8b","linear8c"])
         
-        solverf_nopresolve() = MOIXPR.XpressSolverInstance(PRESOLVE = 0)
-        MOIT.contlineartest(solverf_nopresolve , linconfig, ["linear12"])
+        solver_nopresolve = XpressOptimizer(PRESOLVE = 0)
+        MOIT.contlineartest(solver_nopresolve , linconfig, ["linear12"])
 
         linconfig_nocertificate = MOIT.TestConfig(infeas_certificates=false)
-        MOIT.linear12test(solverf, linconfig_nocertificate)
+        MOIT.linear12test(solver, linconfig_nocertificate)
     end
 
     @testset "Quadratic tests" begin
         quadconfig = MOIT.TestConfig(atol=1e-5, rtol=1e-5, duals=false, query=false)
-        solverf() = MOIXPR.XpressSolverInstance()
-        MOIT.contquadratictest(solverf, quadconfig)
+        solver = XpressOptimizer()
+        MOIT.contquadratictest(solver, quadconfig)
     end
 
     @testset "Linear Conic tests" begin
         linconfig = MOIT.TestConfig()
-        solverf() = MOIXPR.XpressSolverInstance()
-        MOIT.lintest(solverf, linconfig, ["lin3","lin4"])
+        solver = XpressOptimizer()
+        MOIT.lintest(solver, linconfig, ["lin3","lin4"])
 
-        solverf_nopresolve() = MOIXPR.XpressSolverInstance(PRESOLVE=0)
-        MOIT.lintest(solverf_nopresolve, linconfig)
+        solver_nopresolve = XpressOptimizer(PRESOLVE=0)
+        MOIT.lintest(solver_nopresolve, linconfig)
     end
 
     @testset "Integer Linear tests" begin
         intconfig = MOIT.TestConfig()
-        solverf() = MOIXPR.XpressSolverInstance()
-        MOIT.intlineartest(solverf, intconfig)
+        solver = XpressOptimizer()
+        MOIT.intlineartest(solver, intconfig)
     end
 end
 ;
